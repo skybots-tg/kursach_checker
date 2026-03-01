d35a63fd6e719487dc13e08fbfe43a217be950d3
Alex <start.user.load@gmail.com>
2026-03-01 08:59:23 +0300

    init

--- frontend-miniapp/src/main.tsx
+++ frontend-miniapp/src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import "./styles/variables.css";
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/components.css";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
